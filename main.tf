cb5761aadf7a76cd4b417d3e6ec4312c55658c6b
igetit41 <45726571+igetit41@users.noreply.github.com>
2019-03-11 16:20:17 -0700

    Restructure

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ data "external" "connectionVars" {
   program = ["sh", "connectionVars.sh" ]
 }
 
-#Passed in subscription id number 1
+#Subscription01 ID
 variable "subscription_id01" {}
 
-# The default provider configuration for default subscription
+#Provider for Subscription01
 provider "azurerm" {
   alias  = "ct-terraform01"
   subscription_id = "${var.subscription_id01}"
@@ -15,14 +15,15 @@ provider "azurerm" {
   tenant_id       = "${data.external.connectionVars.result.ARM_TENANT_ID}"
 }
 
-#Call environment module for subscription number 1
+#Subscription01_ZoneA
 module "ct-terraform01-module" {
-  source    = "./subscriptionConfigs"
+  source    = "./modules"
   providers = {
     azurerm = "azurerm.ct-terraform01"
   }
   prefix = "ct-terraform01"
   region = "westus2"
+  designation = "pd"
 }
 
 
@@ -34,10 +35,10 @@ module "ct-terraform01-module" {
 
 
 
-#Passed in subscription id number 2
+#Subscription02 ID
 variable "subscription_id02" {}
 
-# Additional provider configuration for second subscription
+#Provider for Subscription02
 provider "azurerm" {
   alias  = "ct-terraform02"
   subscription_id = "${var.subscription_id02}"
@@ -46,12 +47,13 @@ provider "azurerm" {
   tenant_id       = "${data.external.connectionVars.result.ARM_TENANT_ID}"
 }
 
-#Call environment module for subscription number 2
+#Subscription02_ZoneA
 module "ct-terraform02-module" {
-  source    = "./subscriptionConfigs"
+  source    = "./modules"
   providers = {
     azurerm = "azurerm.ct-terraform02"
   }
   prefix = "ct-terraform02"
   region = "eastus2"
+  designation = "pd"
 }
