69237c04778cf59abe87abc33ff48e4d95c5d0df
igetit41 <igetit41@yahoo.com>
2019-03-14 12:04:06 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,9 +9,8 @@ variable "namePrefix" {
 }
 
 
-data "peering_vnet_list" {
-  type = "list"
-  default = []
+local {
+  peering_vnet_list = []
 }
 
 #Subscription01 ID
@@ -38,7 +37,7 @@ module "ct-terraform01-module" {
   address_space = "${list("192.168.0.0/24","192.168.1.0/24")}"
   subnets_names = "${list("GatewaySubnet","Subnet_01")}"
   subnets_address_prefixes = "${list("192.168.0.0/25","192.168.0.128/25")}"
-  var.peering_vnet_list = "${merge(var.namePrefix,module.ct-terraform01-module.azurerm_virtual_network)}"
+  local.peering_vnet_list = "${merge(var.namePrefix,module.ct-terraform01-module.azurerm_virtual_network)}"
 }
 
 
@@ -74,11 +73,11 @@ module "ct-terraform02-module" {
   address_space = "${list("192.168.128.0/24", "192.168.129.0/24")}"
   subnets_names = "${list("GatewaySubnet","Subnet_01")}"
   subnets_address_prefixes = "${list("192.168.128.0/25","192.168.128.128/25")}"
-  var.peering_vnet_list = "${merge(var.namePrefix,module.ct-terraform02-module.azurerm_virtual_network)}"
+  local.peering_vnet_list = "${merge(var.namePrefix,module.ct-terraform02-module.azurerm_virtual_network)}"
 }
 
 
 
 output "output" {
-  value = "${var.peering_vnet_list}"
+  value = "${local.peering_vnet_list}"
 }
